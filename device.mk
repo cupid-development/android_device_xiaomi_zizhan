b2683ebda3b06f8d88301c38c09da43c0238413f
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-20 19:21:09 +0200

    zizhan: Add Xiaomi sensor module
    Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1

--- device.mk
+++ device.mk
@@ -29,6 +29,9 @@ PRODUCT_PACKAGES += \
     SystemUIResZizhan \
     WifiResZizhan
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
