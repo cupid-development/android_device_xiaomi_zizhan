e8ee07529531c69bb1e9663daa6295833e8b7d66
Jens Reidel <adrian@travitia.xyz>
2024-04-23 19:45:58 +0200

    zizhan: Declare hinge angle sensor
    Change-Id: If730c8fd774e952002ee522f65838f5f31cbb237
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- device.mk
+++ device.mk
@@ -15,6 +15,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/display/display_config_inner.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946220589295746.xml \
     $(LOCAL_PATH)/display/display_config_outer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946220589295747.xml
 
+# Hinge angle sensor
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.sensor.hinge_angle.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_cape/android.hardware.sensor.hinge_angle.xml
+
 # Overlay
 PRODUCT_PACKAGES += \
     ApertureResZizhan \
