665bd83837dc5691ab1786aaf184bdeaa6b5a35b
Jens Reidel <adrian@travitia.xyz>
2024-08-15 02:06:23 +0200

    zizhan: Expose aux cameras to Aperture
    Change-Id: I6d22e368f650f062b32129ea3222e6443974944c
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,7 @@ BOOT_KERNEL_MODULES += $(device_second_stage_modules)
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
 # Recovery
 TARGET_RECOVERY_DEFAULT_ROTATION := ROTATION_LEFT
