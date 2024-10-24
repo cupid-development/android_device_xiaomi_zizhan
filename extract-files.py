28b49c19654e0c63cdd876b5e1d35035016ff3cd
Arian <arian.kulmer@web.de>
2024-10-24 23:40:01 +0200

    zizhan: Switch to python extract-utils
    Change-Id: Ia6ea2ea79cb3bcbcbf06f0e6660ac31ea576b524

---
+++ extract-files.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'device/xiaomi/sm8450-common',
+    'hardware/qcom-caf/sm8450',
+    'hardware/xiaomi',
+    'vendor/qcom/opensource/commonsys-intf/display',
+    'vendor/xiaomi/sm8450-common',
+]
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'libagmclient',
+        'libagmmixer',
+        'vendor.qti.hardware.pal@1.0-impl',
+    ): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    (
+        'vendor/etc/camera/zizhan_enhance_motiontuning.xml',
+        'vendor/etc/camera/zizhan_motiontuning.xml',
+    ): blob_fixup().regex_replace('xml=version', 'xml version'),
+    ('vendor/etc/camera/pureView_parameter.xml',): blob_fixup().regex_replace(
+        r'=([0-9]+)>', r'="\1">'
+    ),
+    'vendor/lib64/libcamximageformatutils.so': blob_fixup().replace_needed(
+        'vendor.qti.hardware.display.config-V2-ndk_platform.so',
+        'vendor.qti.hardware.display.config-V2-ndk.so',
+    ),
+}
+
+module = ExtractUtilsModule(
+    'zizhan',
+    'xiaomi',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    check_elf=True,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'sm8450-common', module.vendor
+    )
+    utils.run()
